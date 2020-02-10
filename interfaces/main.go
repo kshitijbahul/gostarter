7d234dd3dd26287eee767558c510528852c9cce1
KshitijBahul <chunks.3@gmail.com>
2020-02-10 13:55:19 +0530

    Added the interface Bot

--- interfaces/main.go
+++ interfaces/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+type bot interface {
+	getGreeting() string //Specifies any other type in the program that has the string getgreeting then that type is a bot. Hence English and Spanish Bots are now of type Bot interface
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -23,11 +27,18 @@ func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
 
+/*
+Commented to write an interface
 func printGreeting(eb englishBot) {
 	//has type specific for english greeting
 	fmt.Println(eb.getGreeting())
 }
+
 func printGreeting(sb spanishBot) {
 	//has type specific for english greeting
 	fmt.Println(sb.getGreeting())
+}
+*/
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
