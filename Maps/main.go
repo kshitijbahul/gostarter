bf71d7ac8aa193fc68f7fc5e285465c1e1dba233
KshitijBahul <chunks.3@gmail.com>
2020-02-10 13:43:42 +0530

    Updated Maps Code

--- Maps/main.go
+++ Maps/main.go
@@ -9,5 +9,19 @@ func main() {
 		"red":   "#ff000",
 		"green": "#ff745",
 	}
+	//var colours map[string]string
+	//colours := make(map[string]string)
+	colours["white"] = "##0000"
+	colours["10"] = "##mkasdasm"
+
+	//fmt.Println(colours)
+	parseMap(colours)
+	delete(colours, "10")
 	fmt.Println(colours)
+}
+
+func parseMap(colour map[string]string) {
+	for key, value := range colour {
+		fmt.Println(key, value)
+	}
 }
